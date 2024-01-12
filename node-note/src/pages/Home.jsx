69ac8d92399d3a5a24431bc0cb27959f7af84ec5
Apiratchai <Apiratchai@kkumail.com>
2024-01-12 18:16:14 +0700

    daisy ui, test page

---
+++ node-note/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='header'>Home this is a home<br></br>there are many like this but this one is mine
+        </div>
+    )
+}
+
+export default Home
--- node-note/src/pages/index.jsx
+++ node-note/src/pages/index.jsx
@@ -1,19 +1,39 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Link from "next/link"
 
 export default function Component() {
   const { data: session } = useSession()
   if (session) {
     return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+      <div className="navbar flex-row bg-base-300">
+        <div className="flex-1 flex-row">
+          {/* Search Input */}
+          <div className="form-control">
+            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+          </div>
+          {/* Home Link */}
+          <Link href="/Home">
+            <div className="btn btn-ghost text-xl">Node-Note</div>
+          </Link>
+        </div>
+
+        {/* Profile */}
+        <div>
+          <div tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-full rounded-full">
+              {session.user.image && (
+                <img src="{session.user.image}" alt="Profile" className="w-full h-full object-cover rounded-full" />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
   return (
     <>
       Not signed in <br />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => signIn()}>Sign in</button>
+      <button className="btn btn-primary btn-xl" onClick={() => signIn()}>Sign in</button>
     </>
   )
 }
