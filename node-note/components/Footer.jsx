2ceaa0cdb5da0b3d584a78c34e76eb8bc292b7b6
Apiratchai <Apiratchai@kkumail.com>
2024-01-20 18:48:47 +0700

    fix tailwind config

--- node-note/components/Footer.jsx
+++ node-note/components/Footer.jsx
@@ -1,13 +1,13 @@
 export default function Footer() {
     return (
-        <div className="flex flex-row justify-center gap-x-10">
+        <div className="flex flex-row justify-center space-x-10">
             <div>
-                <button className="hover:text-green-400 text-whtie transition duration-500">
+                <button className="hover:text-white text-blue-500 transition duration-500">
                     About Us
                 </button>
             </div>
             <div >
-                <button className="hover:text-black text-white transition duration-500">
+                <button className="hover:text-white text-blue-500 transition duration-500">
                     Contact
                 </button>
             </div>
