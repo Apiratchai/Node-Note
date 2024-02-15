67ac731bcdeea02d77a7f07032a9510d15873773
Apiratchai <Apiratchai@kkumail.com>
2024-02-15 19:26:03 +0700

    fix shadcn-ui again (label)

--- node-note/@/components/ui/Label.tsx
+++ node-note/@/components/ui/Label.tsx
@@ -1,8 +1,7 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
-
-import { cn } from "@/lib/utils"
+import classNames from "classnames"
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -15,7 +14,7 @@ const Label = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={cn(labelVariants(), className)}
+    className={classNames(labelVariants(), className)}
     {...props}
   />
 ))
