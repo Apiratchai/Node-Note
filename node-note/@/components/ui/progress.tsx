cd4ac944a4a79149095d62b01fc0507f4d711f82
Apiratchai <Apiratchai@kkumail.com>
2024-02-15 19:33:32 +0700

    fix cn imports to classNames

--- node-note/@/components/ui/progress.tsx
+++ node-note/@/components/ui/progress.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
-
-import { cn } from "@/lib/utils"
+import classNames from "classnames"
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
@@ -9,7 +8,7 @@ const Progress = React.forwardRef<
 >(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={cn(
+    className={classNames(
       "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
       className
     )}
