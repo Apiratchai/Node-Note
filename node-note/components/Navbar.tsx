afdb32d579ca036c9b9da68f53685b222ff33504
Apiratchai <Apiratchai@kkumail.com>
2024-02-25 22:12:49 +0700

    rework navbar

--- node-note/components/Navbar.tsx
+++ node-note/components/Navbar.tsx
@@ -32,21 +32,23 @@ export const Navbar = ({
         </nav>
     }
     if (document === null) {
-        <>
-            <nav className="pl-5 bg-white flex items-center gap-x-4">
-                {isCollapsed && (
-                    <MenuIcon
-                        role="button"
-                        onClick={onResetWidth}
-                        className="h-6 w-6 hover:bg-gray-100" />
-                )}
-                <div className="flex items-center justify-between w-full py-2">
-                    <div>
-                        This will be toggle button
+        return (
+            <>
+                <nav className="pl-5 bg-white flex items-center gap-x-4">
+                    {isCollapsed && (
+                        <MenuIcon
+                            role="button"
+                            onClick={onResetWidth}
+                            className="h-6 w-6 hover:bg-gray-100" />
+                    )}
+                    <div className="flex items-center justify-between w-full py-2">
+                        <div>
+                            This will be toggle button
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </>
+                </nav>
+            </>
+        )
     }
     return (
         <>
