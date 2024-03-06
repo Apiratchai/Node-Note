37921a54e793b32cecd5a2d0d8da683fa5912a11
achitapan <achitapan.s@kkumail.com>
2024-03-06 07:18:31 +0700

    update contact us pop up

--- node-note/components/ContactPopup.jsx
+++ node-note/components/ContactPopup.jsx
@@ -11,20 +11,20 @@ const ContactPopup = ({ open, onClose }) => {
   return (
     <div>
       <div id='container' onClick={handleOnClose} className="fixed inset-0 bg-black bg-opacity-50 backdrop-brightness-50 flex justify-center items-center">
-        <div className='box-content h-[180px] w-[500px] bg-white p-7 rounded'>
+        <div className='box-content h-[180px] w-[750px] bg-white p-7 rounded'>
           <div className="flex flex-col space-y-2">
-            <h1 className="text-3xl mb-8 text-black text-center"> Contact Us </h1>
+            <h1 className="cursor-default text-3xl mb-8 text-black text-center"> Contact Us </h1>
             {[
               { name: "Apiratchai Lakkum", role: "Full stack", facebook: "https://web.facebook.com/profile.php?id=100088070056042", email: "apiratchai.l@kkumail.com" },
               { name: "Kunasin Salabsri", role: "UI designer", facebook: "https://web.facebook.com/kunasin.salabsri.7", email: "kunasin.s@kkumail.com" },
               { name: "Achitapan Sutthiwanna", role: "Front end", facebook: "https://web.facebook.com/A.Sutthivanna", email: "achitapan.s@kkumail.com" }
             ].map((member, index) => (
               <div key={index} className="flex flex-row justify-between items-center">
-                <div className="text-sm md:text-xl text-black">{member.name}</div>
-                <div className="text-xs font-bold text-green-700 absolute ml-40 md:ml-60 mt-1">{member.role}</div>
-                <div className="flex gap-3 test-xs md:text-base">
-                  <a href={member.facebook} className="cursor-pointer  text-blue-500 px-1 hover:text-blue-500 hover:bg-blue-500 hover:bg-opacity-25 text-black transition duration-175">Facebook</a>
-                  <a href={member.email} className="cursor-pointer text-orange-400 px-1 hover:text-orange-500 hover:bg-orange-500 hover:bg-opacity-25 text-black transition duration-175">Email</a>
+                <div className="cursor-default text-sm md:text-xl text-black">{member.name}</div>
+                <div className="cursor-default text-xs font-bold text-green-700 absolute ml-40 md:ml-60 mt-1">{member.role}</div>
+                <div className="flex absolute ml-40 md:ml-96 mt-1 gap-x-10">
+                  <a href={member.facebook} className="cursor-pointer text-blue-500 px-1 hover:text-blue-500 hover:bg-blue-500 hover:bg-opacity-25 transition duration-175">Facebook</a>
+                  <button className="cursor-copy text-orange-400 px-1 hover:text-orange-500 hover:bg-orange-500 hover:bg-opacity-25 transition duration-175">Email: {member.email}</button>
                 </div>
               </div>
             ))}
--- node-note/components/Footer.jsx
+++ node-note/components/Footer.jsx
@@ -1,6 +1,6 @@
+import { useRouter } from "next/router";
 import React, { useState } from 'react';
 import ContactPopup from "./ContactPopup";
-import { useRouter } from "next/router";
 
 
 export default function Footer() {
@@ -13,14 +13,17 @@ export default function Footer() {
         <div className="flex flex-row justify-center ">
 
             <div>
-                <button onClick={goToAboutUsPage} className="text-2xl cursor-pointer px-5 hover:text-white hover:bg-white hover:bg-opacity-25 text-blue-500 transition duration-500">
+                <button onClick={goToAboutUsPage} className="text-2xl cursor-pointer px-5 hover:text-white hover:bg-white hover:bg-opacity-25 text-blue-200 transition duration-500">
                     About Us
                 </button>
             </div>
             <div>
-                <button onClick={() => setOpenContact(true)} className="text-2xl cursor-pointer px-5 hover:text-white hover:bg-white hover:bg-opacity-25 text-blue-500 transition duration-500">
+                <button onClick={() => setOpenContact(true)} className="text-2xl cursor-pointer px-5 hover:text-white hover:bg-white hover:bg-opacity-25 text-blue-200 transition duration-500">
                     Contact Us
                 </button>
+                <div className="absolute invisible group-hover:visible bg-white shadow-md px-4 py-2 rounded-md text-sm">
+                    Hi Hello
+                </div>
                 <ContactPopup open={openContact} onClose={() => setOpenContact(false)} />
             </div>
         </div>
