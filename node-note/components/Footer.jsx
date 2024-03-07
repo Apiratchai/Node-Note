81a43162fd1237c96d231349512099d630f60512
Apiratchai <Apiratchai@kkumail.com>
2024-03-07 16:29:56 +0700

    deleted invisible block component in Footer.jsx

--- node-note/components/Footer.jsx
+++ node-note/components/Footer.jsx
@@ -21,9 +21,6 @@ export default function Footer() {
                 <button onClick={() => setOpenContact(true)} className="text-2xl cursor-pointer px-5 hover:text-white hover:bg-white hover:bg-opacity-25 text-blue-200 transition duration-500">
                     Contact Us
                 </button>
-                <div className="absolute invisible group-hover:visible bg-white shadow-md px-4 py-2 rounded-md text-sm">
-                    Hi Hello
-                </div>
                 <ContactPopup open={openContact} onClose={() => setOpenContact(false)} />
             </div>
         </div>
